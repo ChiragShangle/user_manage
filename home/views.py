18399a28e38af7fc0d5d50bc9764856427505dab
Chirag Shangle <chiragshangle15@gmail.com>
2021-12-30 10:24:29 +0530

    bug

--- home/views.py
+++ home/views.py
@@ -5,7 +5,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib import messages
 from .models import *
-import uuid
 from django.conf import settings
 from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
--- user/settings.py
+++ user/settings.py
@@ -136,5 +136,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'notify.user.management@gmail.com'
-EMAIL_HOST_PASSWORD = 'Usermanagement@1234'
